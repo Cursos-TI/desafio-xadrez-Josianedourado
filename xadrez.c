9307b8a3eacefb887357f558c38561c36d753818
Josianedourado <josiane.dourado62@gmail.com>
2025-04-13 19:01:08 +0000

    novo commit

--- xadrez.c
+++ xadrez.c
@@ -7,8 +7,20 @@ printf("desafio xadrez iniciante\n");
 
 
  for (  int R = 0; R < 8; R++ ){    
-    printf( "esquerda\n");
+    printf( "esquerda\n\n");
  }
+ int B = 1;
+ while(   B <= 5){ 
+   printf("para cima,esquerda\n", B);
+   B++;
+}
+int T = 1;
+do{  
+   printf("direita\n" ,T);
+   T++;
+}while  (  T <= 5);
+
+ 
 
  
 
